9cd5575306d46a40ebe4a5b840273b8c5a0551ce
Sergey68 <sesergey@yandex.ru>
2022-07-03 18:33:45 +1100

    File is updated with Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// программa выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет
+
+Console.WriteLine("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if( number >= 100)
+{
+    Console.Write("Third digit is ");
+    Console.WriteLine(number % 10);
+} 
+else
+{
+    Console.WriteLine(" There is no third digit !!!");
+}
+

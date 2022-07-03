aaa525f0b4c985f2bc24143b4074eb83e50c9a09
Sergey68 <sesergey@yandex.ru>
2022-07-03 20:39:15 +1100

    Task2 is updated with condition "or"

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input day of week N- ");
+int weekDay = int.Parse(Console.ReadLine());
+
+if ( weekDay == 6 || weekDay == 7)
+{
+    Console.WriteLine("This is weekend");
+}
+else
+{
+    Console.WriteLine("This is work day");
+}

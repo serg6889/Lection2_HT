fc6f43967ffe92efd0fe3884603a5ffa30a31ce9
Sergey68 <sesergey@yandex.ru>
2022-07-03 21:57:51 +1100

    File is updated with Task2  Please enter the commit message for your changes. Lines starting

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,13 +4,19 @@
 Console.WriteLine("Input number ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if( number >= 100)
-{
-    Console.Write("Third digit is ");
-    Console.WriteLine(number % 10);
-} 
-else
+if( number < 100)
 {
     Console.WriteLine(" There is no third digit !!!");
+    
+} 
+else if (number >= 100)
+{
+    //Console.WriteLine(number.ToString().Substring(2,1));
+    Console.WriteLine(number.ToString()[2]);
+
 }
+
+
+
+
 

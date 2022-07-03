73bbd183ea7d603c6e73fe3567bc3b35db058488
Sergey68 <sesergey@yandex.ru>
2022-07-03 16:24:18 +1100

    File is updated with Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// программa принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Input 3-digit number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if( number >= 100 && number < 1000)
+{
+    Console.WriteLine(number % 10);
+} 
+else
+{
+    Console.WriteLine("Incorrect input !!!");
+}
